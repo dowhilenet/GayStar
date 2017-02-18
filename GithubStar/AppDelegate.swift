c401a4bf9185b2a84e32d69bbedfefa821c9296a
这块显卡有点冷 <root@dowhile.net>
2017-02-18 10:42:02 +0800

    Author

--- GithubStar/AppDelegate.swift
+++ GithubStar/AppDelegate.swift
@@ -7,36 +7,41 @@
 //
 
 import UIKit
+import SwiftyOAuth
 
-
-
+let github = Provider.gitHub(clientID: "af3689b7eef793657839", clientSecret: "b2f4713ee30029079d036428f0ed45c6b44982a2", redirectURL: "GITStare://callback")
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
     
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: Any]?) -> Bool {
-        
-        window = UIWindow()
-        let rootview = RootTabBarViewController()        
-        window!.rootViewController = rootview
-        window!.makeKeyAndVisible()
+    private func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: Any]?) -> Bool {
+      
+        self.window = UIWindow()
+        let rootview = UIViewController()
+        self.window!.rootViewController = rootview
+        self.window!.backgroundColor = UIColor.white
+        self.window!.makeKeyAndVisible()
 
+        
+//        github.authorize { (result) in
+//            switch result {
+//            case .failure(let error):
+//                print(error.localizedDescription)
+//            case .success(let value):
+//                print(value.accessToken)
+//            }
+//        }
         
         return true
     }
     
 
-    
-    func application(_ application: UIApplication, handleOpen url: URL) -> Bool {
-        if url.host == "oauth-callback"{
-            if url.path.hasPrefix("/github"){
-             OAuth2Swift.handleOpenURL(url)
-            }
-        }
-        return true
-    }
+//    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+//        github.handleURL(url, options: options)
+//        return true
+//    }
     
     
     
--- Podfile
+++ Podfile
@@ -6,19 +6,8 @@ target 'GithubStar' do
   use_frameworks!
 
 pod 'SwiftyJSON'
-pod 'Kingfisher', '~> 3.0'
-pod 'Moya', '8.0.0-beta.2'
-pod 'Moya/RxSwift'
-pod 'RxSwift', '3.0.0-beta.1'
-pod 'Fuzi', '~> 1.0.0'
-pod 'SnapKit', '~> 3.0'
-pod 'SwiftyUserDefaults'
-pod 'DGRunkeeperSwitch', '~> 1.1.4'
-pod 'DGElasticPullToRefresh'
-
-
-#pod 'OAuthSwift', :git => 'https://github.com/OAuthSwift/OAuthSwift.git', :branch => 'swift3.0'
-
-# OC
-pod 'JSQMessagesViewController'
+pod 'Kingfisher'
+pod 'Moya'
+pod 'SnapKit'
+pod 'SwiftyOAuth'
 end
